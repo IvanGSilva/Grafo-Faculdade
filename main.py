841df45f1c0bd4fd8fcd7353fdd4e054953c4129
IvanGSilva <119447600+IvanGSilva@users.noreply.github.com>
2024-11-13 20:43:40 -0300

    -mod: implementação do algoritmo AStar no demonstrativo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gbfSearch import GBFSearch
+from aStar import AStar
 from graph import Graph
 from dfs import Dfs
 from bfs import Bfs
@@ -6,17 +7,18 @@ from bfs import Bfs
 
 graph = Graph()
 gbf_search = GBFSearch(graph)
+a_star_search = AStar(graph)
 
 dfsPath = Dfs(graph).dfs(graph.arad)
 
 bfsPath = Bfs(graph).bfs(graph.arad)
 
-print(dfsPath)
+print("Depth Firist Search",dfsPath)
 print("\n"
   "--------------------------------------------------------\n"
   "\n")
-print(bfsPath)
 
+print("Bredth Firist Path",bfsPath)
 print("\n"
   "--------------------------------------------------------\n"
   "\n")
@@ -27,6 +29,17 @@ goal_vertex = graph.bucharest
 path = gbf_search.greedy_bfs(start_vertex, goal_vertex)
 
 if path:
-    print("Caminho encontrado:", path)
+    print("Caminho encontrado por Greedy-Best-Firist-Search:", path)
+else:
+    print("Objetivo não encontrado.")
+
+print("\n"
+  "--------------------------------------------------------\n"
+  "\n")
+
+path = a_star_search.a_star(start_vertex, goal_vertex)
+
+if path:
+    print("Caminho encontrado por A-Star:", path)
 else:
     print("Objetivo não encontrado.")
